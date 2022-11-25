61f2618f4c96e73a03e580ea846ee820d6557e85
Maximov <m@m.com>
2022-11-25 13:38:48 -0500

    Final 1.14

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -3,11 +3,18 @@ package hexlet.code.schemas;
 public abstract class BaseSchema {
     boolean requiredRule;
 
+    /**
+     * @return turn on validation for null
+     */
     public BaseSchema required() {
         requiredRule = true;
         return this;
     }
 
+    /**
+     * @param objText object for null validation
+     * @return true if not null
+     */
     protected boolean isValid(Object objText) {
         return !requiredRule || objText != null;
     }
